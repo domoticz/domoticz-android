8510e5d1e41f25710e682605e9efd41723774f6d
galadril <heinis.mark@gmail.com>
2015-12-16 17:50:46 +0100

    v0.1.155
    - Added some extra custom icons
    - Fixed several bugs

--- app/src/main/java/nl/hnogames/domoticz/Domoticz/Domoticz.java
+++ app/src/main/java/nl/hnogames/domoticz/Domoticz/Domoticz.java
@@ -794,9 +794,59 @@ public class Domoticz {
                 url);
     }
 
-    public int getDrawableIcon(String type, String subtype, boolean state) {
+
+    public int getDrawableIcon(String imgType, String Type , String switchType, boolean State, boolean useCustomImage, String CustomImage) {
+        int standardImage = getDrawableIcon(imgType, Type, switchType, State);
+
+        if(useCustomImage && CustomImage!=null && CustomImage.length()>0)
+        {
+            switch (CustomImage) {
+                case "Alarm":
+                    return R.drawable.alarm;
+                case "Amplifier":
+                    return R.drawable.volume;
+                case "Computer":
+                case "ComputerPC":
+                    return R.drawable.computer;
+                case "Cooling":
+                    return R.drawable.cooling;
+                case "ChristmasTree":
+                    return R.drawable.christmastree;
+                case "Fan":
+                    return R.drawable.wind;
+                case "Fireplace":
+                    return R.drawable.flame;
+                case "Generic":
+                    return R.drawable.generic;
+                case "Harddisk":
+                    return R.drawable.harddisk;
+                case "Heating":
+                    return R.drawable.heating;
+                case "Light":
+                    return R.drawable.lights;
+                case "Media":
+                    return R.drawable.video;
+                case "Phone":
+                    return R.drawable.phone;
+                case "Speaker":
+                    return R.drawable.sub;
+                case "Printer":
+                    return R.drawable.printer;
+                case "TV":
+                    return R.drawable.tv;
+                case "WallSocket":
+                    return R.drawable.wall;
+                case "Water":
+                    return R.drawable.water;
+            }
+        }
+
+        return standardImage;
+    }
+
+    private int getDrawableIcon(String imgType, String Type , String switchType, boolean State) {
         int test = R.drawable.defaultimage;
-        switch (type.toLowerCase()) {
+        switch (imgType.toLowerCase()) {
             case "scene":
                 return R.drawable.generic;
             case "group":
@@ -808,8 +858,8 @@ public class Domoticz {
             case "door":
                 return R.drawable.door;
             case "lightbulb":
-                if (subtype != null && subtype.length() > 0 && subtype.equals(Device.Type.Name.DUSKSENSOR))
-                    if (state)
+                if (switchType != null && switchType.length() > 0 && switchType.equals(Device.Type.Name.DUSKSENSOR))
+                    if (State)
                         return R.drawable.uvdark;
                     else
                         return R.drawable.uvsunny;
@@ -834,7 +884,7 @@ public class Domoticz {
             case "blinds":
                 return R.drawable.down;
             case "dimmer":
-                if (subtype != null && subtype.length() > 0 && subtype.startsWith(Device.SubType.Name.RGB))
+                if (switchType != null && switchType.length() > 0 && switchType.startsWith(Device.SubType.Name.RGB))
                     return R.drawable.rgb;
                 else
                     return R.drawable.dimmer;
@@ -843,12 +893,12 @@ public class Domoticz {
             case "security":
                 return R.drawable.security;
             case "temperature":
-                if (state)
+                if (State)
                     return R.drawable.heating;
                 else
                     return R.drawable.cooling;
             case "counter":
-                if (subtype != null && subtype.length() > 0 && subtype.equals("P1 Smart Meter"))
+                if (Type != null && Type.length() > 0 && Type.equals("P1 Smart Meter"))
                     return R.drawable.wall;
                 else
                     return R.drawable.up;
@@ -859,6 +909,7 @@ public class Domoticz {
             case "radiation":
                 return R.drawable.radiation;
             case "moisture":
+            case "rain":
                 return R.drawable.rain;
             case "leaf":
                 return R.drawable.leaf;
