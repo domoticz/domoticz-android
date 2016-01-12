12cb51fcf4a1a5ee6861ff5e573adb33be941748
nl.inversion@gmail.com <nl.inversion@gmail.com>
2016-01-12 08:56:15 +0100

    Revert "changes to thermostat dialog"
    This reverts commit 95e832ba23c7a6a371f41f486047117bba7e74d9.

--- app/src/main/java/nl/hnogames/domoticz/UI/TemperatureDialog.java
+++ app/src/main/java/nl/hnogames/domoticz/UI/TemperatureDialog.java
@@ -25,65 +25,67 @@ package nl.hnogames.domoticz.UI;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Build;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.nineoldandroids.animation.ValueAnimator;
 import com.triggertrap.seekarc.SeekArc;
 
 import nl.hnogames.domoticz.R;
+import nl.hnogames.domoticz.Utils.SharedPrefUtil;
 
 public class TemperatureDialog implements DialogInterface.OnDismissListener {
 
     private final MaterialDialog.Builder mdb;
     private DismissListener dismissListener;
     private Context mContext;
+    private SharedPrefUtil mSharedPrefs;
     private int idx;
-    private int currentTemperature = 200;
+    private double currentTemperature = 20;
     private SeekArc temperatureControl;
     private TextView temperatureText;
+    private Button bntPlus, btnMin;
 
     public TemperatureDialog(Context mContext, int idx, double temp) {
         this.mContext = mContext;
+        mSharedPrefs = new SharedPrefUtil(mContext);
         this.idx = idx;
         mdb = new MaterialDialog.Builder(mContext);
         mdb.customView(R.layout.dialog_temperature, false)
                 .positiveText(android.R.string.ok);
         mdb.dismissListener(this);
-        currentTemperature = (int) temp * 10;
+        currentTemperature = temp;
     }
 
     public void show() {
-
         mdb.title(mContext.getString(R.string.temperature));
         final MaterialDialog md = mdb.build();
         View view = md.getCustomView();
 
         temperatureControl = (SeekArc) view.findViewById(R.id.seekTemperature);
         temperatureText = (TextView) view.findViewById(R.id.seekTempProgress);
-        Button bntPlus = (Button) view.findViewById(R.id.plus);
-        Button btnMin = (Button) view.findViewById(R.id.min);
-        float temp = currentTemperature / 10;
-        temperatureText.setText(String.valueOf(temp));
+        bntPlus = (Button) view.findViewById(R.id.plus);
+        btnMin = (Button) view.findViewById(R.id.min);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
-            ObjectAnimator animation =
-                    ObjectAnimator.ofFloat(temperatureControl, "progress", currentTemperature);
-            animation.setDuration(1000); // 1 second
+        temperatureText.setText(String.valueOf(currentTemperature));
+        int progress = (int) (currentTemperature * 2);
+
+        if(android.os.Build.VERSION.SDK_INT >= 11){
+            ObjectAnimator animation = ObjectAnimator.ofInt(temperatureControl, "progress", progress);
+            animation.setDuration(1000); // 0.5 second
             animation.setInterpolator(new DecelerateInterpolator());
             animation.start();
         }
         else
-            temperatureControl.setProgress(currentTemperature); // no animation on Honeycomb or lower
+            temperatureControl.setProgress(progress); // no animation on Gingerbread or lower
 
         temperatureControl.setOnSeekArcChangeListener(new SeekArc.OnSeekArcChangeListener() {
             @Override
-            public void onProgressChanged(SeekArc seekArc, int progress, boolean b) {
-                currentTemperature = progress;
-                float temp = progress / 10;
+            public void onProgressChanged(SeekArc seekArc, int i, boolean b) {
+                double temp = ((double) temperatureControl.getProgress() / 2);
                 temperatureText.setText(String.valueOf(temp));
             }
 
@@ -99,15 +101,15 @@ public class TemperatureDialog implements DialogInterface.OnDismissListener {
         bntPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentTemperature-=5;
-                temperatureControl.setProgress(currentTemperature + 5);
+
+                temperatureControl.setProgress(temperatureControl.getProgress() + 1);
             }
         });
         btnMin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentTemperature-=5;
-                temperatureControl.setProgress(currentTemperature - 5);
+
+                temperatureControl.setProgress(temperatureControl.getProgress() - 1);
             }
         });
         md.show();
@@ -116,7 +118,7 @@ public class TemperatureDialog implements DialogInterface.OnDismissListener {
     @Override
     public void onDismiss(DialogInterface dialogInterface) {
         if (dismissListener != null)
-            dismissListener.onDismiss(((double) temperatureControl.getProgress() / 10));
+            dismissListener.onDismiss(((double) temperatureControl.getProgress() / 2));
     }
 
     public void onDismissListener(DismissListener dismissListener) {
