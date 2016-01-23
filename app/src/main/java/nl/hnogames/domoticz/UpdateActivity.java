24642632c04ad9276c742ee002e200bafc48e5e5
nl.inversion@gmail.com <nl.inversion@gmail.com>
2016-01-23 18:56:42 +0100

    Update Domoticz Server v2

--- app/src/main/java/nl/hnogames/domoticz/UpdateActivity.java
+++ app/src/main/java/nl/hnogames/domoticz/UpdateActivity.java
@@ -1,13 +1,22 @@
 package nl.hnogames.domoticz;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import nl.hnogames.domoticz.Domoticz.Domoticz;
+import nl.hnogames.domoticz.Interfaces.UpdateReceiver;
+import nl.hnogames.domoticz.Interfaces.VersionReceiver;
 import nl.hnogames.domoticz.Utils.SharedPrefUtil;
+import nl.hnogames.domoticz.Utils.UsefulBits;
 
 public class UpdateActivity extends AppCompatActivity {
+
+    private String TAG = UpdateActivity.class.getSimpleName();
 
     private SharedPrefUtil mSharedPrefs;
     private Domoticz domoticz;
@@ -33,5 +42,61 @@ public class UpdateActivity extends AppCompatActivity {
 
         currentServerVersionValue.setText(mSharedPrefs.getUpdateVersionAvailable());
 
+    }
+
+    private void checkDomoticzServerUpdate() {
+
+        // Get latest Domoticz version
+        domoticz.getUpdate(new UpdateReceiver() {
+            @Override
+            public void onReceiveUpdate(String version, boolean haveUpdate) {
+                // Write update version to shared preferences
+                mSharedPrefs.setUpdateVersionAvailable(version);
+                mSharedPrefs.setServerUpdateAvailable(haveUpdate);
+            }
+
+            @Override
+            public void onError(Exception error) {
+                String message = String.format(
+                        getString(R.string.error_couldNotCheckForUpdates),
+                        domoticz.getErrorMessage(error));
+                showSimpleSnackbar(message);
+                mSharedPrefs.setUpdateVersionAvailable("");
+            }
+        });
+    }
+
+    private String getServerVersion() {
+        domoticz.getVersion(new VersionReceiver() {
+            @Override
+            public void onReceiveVersion(String serverVersion) {
+                if (!UsefulBits.isEmpty(serverVersion)) {
+                    String[] version
+                            = serverVersion.split("\\.");
+                    // Update version is only revision number
+                    String updateVersion =
+                            version[0] + "." + mSharedPrefs.getUpdateVersionAvailable();
+                    String message
+                            = String.format(getString(R.string.update_available),
+                            serverVersion,
+                            updateVersion);
+                    return message;
+                }
+            }
+
+            @Override
+            public void onError(Exception error) {
+                String message = String.format(
+                        getString(R.string.error_couldNotCheckForUpdates),
+                        domoticz.getErrorMessage(error));
+                showSimpleSnackbar(message);
+            }
+        });
+    }
+
+    private void showSimpleSnackbar(String message) {
+        CoordinatorLayout fragmentCoordinatorLayout =
+                (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        Snackbar.make(fragmentCoordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 }
