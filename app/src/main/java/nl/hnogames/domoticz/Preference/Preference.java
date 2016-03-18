68f8827980ac41806e234a1a49d7940c6eb5cb14
Inversion NL <nl.inversion@gmail.com>
2016-03-18 13:47:18 +0100

    Correct premium message on Always On preference

--- app/src/main/java/nl/hnogames/domoticz/Preference/Preference.java
+++ app/src/main/java/nl/hnogames/domoticz/Preference/Preference.java
@@ -289,7 +289,7 @@ public class Preference extends PreferenceFragment {
             @Override
             public boolean onPreferenceChange(android.preference.Preference preference, Object newValue) {
                 if (BuildConfig.LITE_VERSION) {
-                    Toast.makeText(mContext, getString(R.string.category_wear) + " " + getString(R.string.premium_feature), Toast.LENGTH_LONG).show();
+                    Toast.makeText(mContext, getString(R.string.always_on_title) + " " + getString(R.string.premium_feature), Toast.LENGTH_LONG).show();
                     return false;
                 }
                 return true;
