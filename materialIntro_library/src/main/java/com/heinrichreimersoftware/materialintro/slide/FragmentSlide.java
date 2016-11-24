4fc23e2482872158ffde5bef881e4870557fbfe2
Mark Heinis <heinis.mark@gmail.com>
2016-11-24 10:08:20 +0100

    Updated Material Intro to 1.6
    Updated Material Intro to 1.6

--- materialIntro_library/src/main/java/com/heinrichreimersoftware/materialintro/slide/FragmentSlide.java
+++ materialIntro_library/src/main/java/com/heinrichreimersoftware/materialintro/slide/FragmentSlide.java
@@ -1,32 +1,56 @@
 package com.heinrichreimersoftware.materialintro.slide;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.StringRes;
 import android.support.annotation.StyleRes;
+import android.support.v4.app.Fragment;
 import android.support.v7.view.ContextThemeWrapper;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentSlide extends Slide{
+import com.heinrichreimersoftware.materialintro.app.ButtonCtaFragment;
+import com.heinrichreimersoftware.materialintro.app.SlideFragment;
+import com.heinrichreimersoftware.materialintro.view.parallax.ParallaxFragment;
 
-    private final android.support.v4.app.Fragment fragment;
+public class FragmentSlide implements Slide, RestorableSlide, ButtonCtaSlide {
+
+    private Fragment fragment;
     @ColorRes
     private final int background;
     @ColorRes
     private final int backgroundDark;
+    private final boolean canGoForward;
+    private final boolean canGoBackward;
+    private CharSequence buttonCtaLabel = null;
+    @StringRes
+    private int buttonCtaLabelRes = 0;
+    private View.OnClickListener buttonCtaClickListener = null;
 
-    private FragmentSlide(Builder builder) {
+    protected FragmentSlide(Builder builder) {
         fragment = builder.fragment;
         background = builder.background;
         backgroundDark = builder.backgroundDark;
+        canGoForward = builder.canGoForward;
+        canGoBackward = builder.canGoBackward;
+        buttonCtaLabel = builder.buttonCtaLabel;
+        buttonCtaLabelRes = builder.buttonCtaLabelRes;
+        buttonCtaClickListener = builder.buttonCtaClickListener;
     }
 
     @Override
-    public android.support.v4.app.Fragment getFragment() {
+    public Fragment getFragment() {
         return fragment;
+    }
+
+    @Override
+    public void setFragment(Fragment fragment) {
+        this.fragment = fragment;
     }
 
     @Override
@@ -39,25 +63,104 @@ public class FragmentSlide extends Slide{
         return backgroundDark;
     }
 
+    @Override
+    public boolean canGoForward() {
+        if (getFragment() instanceof SlideFragment) {
+            return ((SlideFragment) getFragment()).canGoForward();
+        }
+        return canGoForward;
+    }
+
+    @Override
+    public boolean canGoBackward() {
+        if (getFragment() instanceof SlideFragment) {
+            return ((SlideFragment) getFragment()).canGoBackward();
+        }
+        return canGoBackward;
+    }
+
+    @Override
+    public View.OnClickListener getButtonCtaClickListener() {
+        if (getFragment() instanceof ButtonCtaFragment) {
+            return ((ButtonCtaFragment) getFragment()).getButtonCtaClickListener();
+        }
+        return buttonCtaClickListener;
+    }
+
+    @Override
+    public CharSequence getButtonCtaLabel() {
+        if (getFragment() instanceof ButtonCtaFragment) {
+            return ((ButtonCtaFragment) getFragment()).getButtonCtaLabel();
+        }
+        return buttonCtaLabel;
+    }
+
+    @Override
+    public int getButtonCtaLabelRes() {
+        if (getFragment() instanceof ButtonCtaFragment) {
+            return ((ButtonCtaFragment) getFragment()).getButtonCtaLabelRes();
+        }
+        return buttonCtaLabelRes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FragmentSlide that = (FragmentSlide) o;
+
+        if (background != that.background) return false;
+        if (backgroundDark != that.backgroundDark) return false;
+        if (canGoForward != that.canGoForward) return false;
+        if (canGoBackward != that.canGoBackward) return false;
+        if (buttonCtaLabelRes != that.buttonCtaLabelRes) return false;
+        if (fragment != null ? !fragment.equals(that.fragment) : that.fragment != null)
+            return false;
+        if (buttonCtaLabel != null ? !buttonCtaLabel.equals(that.buttonCtaLabel) : that.buttonCtaLabel != null)
+            return false;
+        return buttonCtaClickListener != null ? buttonCtaClickListener.equals(that.buttonCtaClickListener) : that.buttonCtaClickListener == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fragment != null ? fragment.hashCode() : 0;
+        result = 31 * result + background;
+        result = 31 * result + backgroundDark;
+        result = 31 * result + (canGoForward ? 1 : 0);
+        result = 31 * result + (canGoBackward ? 1 : 0);
+        result = 31 * result + (buttonCtaLabel != null ? buttonCtaLabel.hashCode() : 0);
+        result = 31 * result + buttonCtaLabelRes;
+        result = 31 * result + (buttonCtaClickListener != null ? buttonCtaClickListener.hashCode() : 0);
+        return result;
+    }
+
     public static class Builder{
-        private android.support.v4.app.Fragment fragment;
+        private Fragment fragment;
         @ColorRes
         private int background;
         @ColorRes
         private int backgroundDark = 0;
+        private boolean canGoForward = true;
+        private boolean canGoBackward = true;
+        private CharSequence buttonCtaLabel = null;
+        @StringRes
+        private int buttonCtaLabelRes = 0;
+        private View.OnClickListener buttonCtaClickListener = null;
 
-        public Builder fragment(android.support.v4.app.Fragment fragment) {
+        public Builder fragment(Fragment fragment) {
             this.fragment = fragment;
             return this;
         }
 
         public Builder fragment(@LayoutRes int layoutRes, @StyleRes int themeRes) {
-            this.fragment = Fragment.newInstance(layoutRes, themeRes);
+            this.fragment = FragmentSlideFragment.newInstance(layoutRes, themeRes);
             return this;
         }
 
         public Builder fragment(@LayoutRes int layoutRes) {
-            this.fragment = Fragment.newInstance(layoutRes);
+            this.fragment = FragmentSlideFragment.newInstance(layoutRes);
             return this;
         }
 
@@ -71,6 +174,45 @@ public class FragmentSlide extends Slide{
             return this;
         }
 
+        public Builder canGoForward(boolean canGoForward) {
+            this.canGoForward = canGoForward;
+            return this;
+        }
+
+        public Builder canGoBackward(boolean canGoBackward) {
+            this.canGoBackward = canGoBackward;
+            return this;
+        }
+
+        public Builder buttonCtaLabel(CharSequence buttonCtaLabel) {
+            this.buttonCtaLabel = buttonCtaLabel;
+            this.buttonCtaLabelRes = 0;
+            return this;
+        }
+
+        public Builder buttonCtaLabelHtml(String buttonCtaLabelHtml) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                this.buttonCtaLabel = Html.fromHtml(buttonCtaLabelHtml, Html.FROM_HTML_MODE_LEGACY);
+            }
+            else {
+                //noinspection deprecation
+                this.buttonCtaLabel = Html.fromHtml(buttonCtaLabelHtml);
+            }
+            this.buttonCtaLabelRes = 0;
+            return this;
+        }
+
+        public Builder buttonCtaLabel(@StringRes int buttonCtaLabelRes) {
+            this.buttonCtaLabelRes = buttonCtaLabelRes;
+            this.buttonCtaLabel = null;
+            return this;
+        }
+
+        public Builder buttonCtaClickListener(View.OnClickListener buttonCtaClickListener) {
+            this.buttonCtaClickListener = buttonCtaClickListener;
+            return this;
+        }
+
         public FragmentSlide build(){
             if(background == 0 || fragment == null)
                 throw new IllegalArgumentException("You must set at least a fragment and background.");
@@ -78,35 +220,39 @@ public class FragmentSlide extends Slide{
         }
     }
 
-    public static class Fragment extends android.support.v4.app.Fragment{
+    public static class FragmentSlideFragment extends ParallaxFragment {
         private static final String ARGUMENT_LAYOUT_RES =
                 "com.heinrichreimersoftware.materialintro.SimpleFragment.ARGUMENT_LAYOUT_RES";
         private static final String ARGUMENT_THEME_RES =
                 "com.heinrichreimersoftware.materialintro.SimpleFragment.ARGUMENT_THEME_RES";
 
-        public Fragment() {
+        public FragmentSlideFragment() {
         }
 
-        public static Fragment newInstance(@LayoutRes int layoutRes, @StyleRes int themeRes) {
-            Fragment fragment = new Fragment();
-
+        public static FragmentSlideFragment newInstance(@LayoutRes int layoutRes, @StyleRes int themeRes) {
             Bundle arguments = new Bundle();
             arguments.putInt(ARGUMENT_LAYOUT_RES, layoutRes);
             arguments.putInt(ARGUMENT_THEME_RES, themeRes);
-            fragment.setArguments(arguments);
 
+            FragmentSlideFragment fragment = new FragmentSlideFragment();
+            fragment.setArguments(arguments);
             return fragment;
         }
 
-        public static Fragment newInstance(@LayoutRes int layoutRes) {
-            return newInstance(layoutRes, -1);
+        public static FragmentSlideFragment newInstance(@LayoutRes int layoutRes) {
+            return newInstance(layoutRes, 0);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            Context contextThemeWrapper = new ContextThemeWrapper(getActivity(),
-                    getArguments().getInt(ARGUMENT_THEME_RES));
+            int themeRes = getArguments().getInt(ARGUMENT_THEME_RES);
+            Context contextThemeWrapper;
+            if (themeRes != 0) {
+                contextThemeWrapper = new ContextThemeWrapper(getActivity(), themeRes);
+            } else {
+                contextThemeWrapper = getActivity();
+            }
             LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
 
             return localInflater.inflate(getArguments().getInt(ARGUMENT_LAYOUT_RES), container, false);
--- materialIntro_library/src/main/java/com/heinrichreimersoftware/materialintro/view/FadeableViewPager.java
+++ materialIntro_library/src/main/java/com/heinrichreimersoftware/materialintro/view/FadeableViewPager.java
@@ -3,13 +3,14 @@ package com.heinrichreimersoftware.materialintro.view;
 import android.content.Context;
 import android.database.DataSetObserver;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FadeableViewPager extends ViewPager {
+public class FadeableViewPager extends SwipeBlockableViewPager {
+
     public FadeableViewPager(Context context) {
         super(context);
     }
@@ -25,7 +26,8 @@ public class FadeableViewPager extends ViewPager {
 
     @Override
     public PagerAdapter getAdapter() {
-        return ((PagerAdapterWrapper) super.getAdapter()).getAdapter();
+        PagerAdapterWrapper wrapper = (PagerAdapterWrapper) super.getAdapter();
+        return wrapper == null ? null : wrapper.getAdapter();
     }
 
     @SuppressWarnings("deprecation")
@@ -36,12 +38,12 @@ public class FadeableViewPager extends ViewPager {
     }
 
     @Override
-    public void addOnPageChangeListener(OnPageChangeListener listener) {
+    public void addOnPageChangeListener(@NonNull OnPageChangeListener listener) {
         super.addOnPageChangeListener(new OnPageChangeListenerWrapper(listener));
     }
 
     @Override
-    public void removeOnPageChangeListener(OnPageChangeListener listener) {
+    public void removeOnPageChangeListener(@NonNull OnPageChangeListener listener) {
         super.removeOnPageChangeListener(new OnPageChangeListenerWrapper(listener));
     }
 
@@ -49,7 +51,6 @@ public class FadeableViewPager extends ViewPager {
     public void setPageTransformer(boolean reverseDrawingOrder, PageTransformer transformer) {
         super.setPageTransformer(reverseDrawingOrder, new PageTransformerWrapper(transformer, getAdapter()));
     }
-
 
     private class OnPageChangeListenerWrapper implements OnPageChangeListener{
         private final OnPageChangeListener listener;
@@ -62,7 +63,7 @@ public class FadeableViewPager extends ViewPager {
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
             int count = listener instanceof OnOverscrollPageChangeListener ?
                     FadeableViewPager.super.getAdapter().getCount() : getAdapter().getCount();
-            listener.onPageScrolled(Math.min(position, count),
+            listener.onPageScrolled(Math.min(position, count - 1),
                     position < count ? positionOffset : 0,
                     position < count ? positionOffsetPixels : 0);
         }
@@ -71,7 +72,7 @@ public class FadeableViewPager extends ViewPager {
         public void onPageSelected(int position) {
             int count = listener instanceof OnOverscrollPageChangeListener ?
                     FadeableViewPager.super.getAdapter().getCount() : getAdapter().getCount();
-            listener.onPageSelected(Math.min(position, count));
+            listener.onPageSelected(Math.min(position, count - 1));
         }
 
         @Override
@@ -109,7 +110,7 @@ public class FadeableViewPager extends ViewPager {
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return adapter.isViewFromObject(view, object);
+            return object != null && adapter.isViewFromObject(view, object);
         }
 
         @Override
@@ -218,7 +219,7 @@ public class FadeableViewPager extends ViewPager {
         public float getPageWidth(int position) {
             if(position < adapter.getCount())
                 return adapter.getPageWidth(position);
-            return 1.f;
+            return 1f;
         }
     }
 
