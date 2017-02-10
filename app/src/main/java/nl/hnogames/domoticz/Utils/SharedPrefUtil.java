c4571f9c92994d7a844c95c04d694f0261b4249b
HNO Mobile <heinis.mark@gmail.com>
2017-02-10 16:07:21 +0100

    Update location picker
    Update location picker

--- app/src/main/java/nl/hnogames/domoticz/Utils/SharedPrefUtil.java
+++ app/src/main/java/nl/hnogames/domoticz/Utils/SharedPrefUtil.java
@@ -720,6 +720,7 @@ public class SharedPrefUtil {
 
     public boolean isAPKValidated() {
         return true;//prefs.getBoolean(PREF_APK_VALIDATED, true);
+
     }
 
     public void setAPKValidated(boolean valid) {
