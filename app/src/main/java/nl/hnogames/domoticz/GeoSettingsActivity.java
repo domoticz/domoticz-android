b77dd2404d51365a8275d476c2a7d1b95d5277ea
HNO Mobile <heinis.mark@gmail.com>
2017-04-15 15:57:30 +0200

    Also ask for storage permissions for geofences
    Also ask for storage permissions for geofences

--- app/src/main/java/nl/hnogames/domoticz/GeoSettingsActivity.java
+++ app/src/main/java/nl/hnogames/domoticz/GeoSettingsActivity.java
@@ -123,9 +123,16 @@ public class GeoSettingsActivity extends AppCompatActivity implements OnPermissi
                             permissionHelper
                                     .request(PermissionsUtil.INITIAL_LOCATION_PERMS);
                         } else {
-                            mSharedPrefs.setGeofenceEnabled(isChecked);
-                            oGeoUtils.enableGeoFenceService();
-                            invalidateOptionsMenu();
+                            if (!PermissionsUtil.canAccessStorage(GeoSettingsActivity.this)) {
+                                geoSwitch.setChecked(false);
+                                permissionHelper
+                                        .request(PermissionsUtil.INITIAL_STORAGE_PERMS);
+                            }else {
+                                //all settings are correct
+                                mSharedPrefs.setGeofenceEnabled(isChecked);
+                                oGeoUtils.enableGeoFenceService();
+                                invalidateOptionsMenu();
+                            }
                         }
                     } else {
                         mSharedPrefs.setGeofenceEnabled(isChecked);
@@ -200,7 +207,6 @@ public class GeoSettingsActivity extends AppCompatActivity implements OnPermissi
 
     private void createListView() {
         locations = mSharedPrefs.getLocations();
-        boolean addressChanged = false;
         adapter = new LocationAdapter(this, locations, new LocationClickListener() {
             @Override
             public boolean onEnableClick(LocationInfo locationInfo, boolean checked) {
@@ -477,9 +483,14 @@ public class GeoSettingsActivity extends AppCompatActivity implements OnPermissi
     public void onPermissionGranted(@NonNull String[] permissionName) {
         Log.i("onPermissionGranted", "Permission(s) " + Arrays.toString(permissionName) + " Granted");
         if (PermissionsUtil.canAccessLocation(GeoSettingsActivity.this)) {
-            mSharedPrefs.setGeofenceEnabled(true);
-            oGeoUtils.enableGeoFenceService();
-            invalidateOptionsMenu();
+            if (PermissionsUtil.canAccessStorage(GeoSettingsActivity.this)) {
+                mSharedPrefs.setGeofenceEnabled(true);
+                oGeoUtils.enableGeoFenceService();
+                invalidateOptionsMenu();
+            }else{
+                permissionHelper
+                        .request(PermissionsUtil.INITIAL_STORAGE_PERMS);
+            }
         }
     }
 }
