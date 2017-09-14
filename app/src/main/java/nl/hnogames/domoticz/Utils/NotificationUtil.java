2e16ccf1cbb5010f14ca23edbdd7fd09a4ec1c09
HNO Mobile <heinis.mark@gmail.com>
2017-09-14 23:30:34 +0200

    Fix notifications on Android O  #432
    Fix notifications on Android O  #432

--- app/src/main/java/nl/hnogames/domoticz/Utils/NotificationUtil.java
+++ app/src/main/java/nl/hnogames/domoticz/Utils/NotificationUtil.java
@@ -21,15 +21,16 @@
 
 package nl.hnogames.domoticz.Utils;
 
-import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.RemoteInput;
 import android.util.Log;
 
@@ -46,11 +47,12 @@ import nl.hnogames.domoticz.Service.StopAlarmButtonListener;
 public class NotificationUtil {
 
     public static final String MESSAGE_CONVERSATION_ID_KEY = "conversaton_key";
-    public static final String VOICE_REPLY_KEY = "voice_reply_key";
+    private static final String VOICE_REPLY_KEY = "voice_reply_key";
     private final static String GROUP_KEY_NOTIFICATIONS = "domoticz_notifications";
     private static final String MESSAGE_READ_ACTION = "nl.hnogames.domoticz.Service.ACTION_MESSAGE_READ";
     private static final String MESSAGE_REPLY_ACTION = "nl.hnogames.domoticz.Service.ACTION_MESSAGE_REPLY";
     private static final String UNREAD_CONVERSATION_BUILDER_NAME = "Domoticz -";
+
     private static SharedPrefUtil prefUtil;
     private static int NOTIFICATION_ID = 12345;
 
@@ -68,36 +70,29 @@ public class NotificationUtil {
         prefUtil.addUniqueReceivedNotification(loggedNotification);
         prefUtil.addLoggedNotification(new SimpleDateFormat("yyyy-MM-dd hh:mm ").format(new Date()) + loggedNotification);
 
-        int prio = Notification.PRIORITY_DEFAULT;
-        switch (priority) {
-            case 1:
-                prio = Notification.PRIORITY_HIGH;
-                break;
-            case 2:
-                prio = Notification.PRIORITY_MAX;
-                break;
-            case -1:
-                prio = Notification.PRIORITY_LOW;
-                break;
-            case -2:
-                prio = Notification.PRIORITY_MIN;
-                break;
-        }
-
         List<String> suppressedNot = prefUtil.getSuppressedNotifications();
         List<String> alarmNot = prefUtil.getAlarmNotifications();
         try {
             if (prefUtil.isNotificationsEnabled() && suppressedNot != null && !suppressedNot.contains(text)) {
+                NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                String CHANNEL_ID = "domoticz_channel";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                            context.getString(R.string.category_notification),
+                            GetPriority(priority));
+                    mNotificationManager.createNotificationChannel(channel);
+                }
+
                 NotificationCompat.Builder builder =
                         new NotificationCompat.Builder(context)
                                 .setSmallIcon(R.drawable.domoticz_white)
                                 .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher))
                                 .setContentTitle(alarmNot != null && alarmNot.contains(loggedNotification) ? context.getString(R.string.alarm) + ": " + title : title)
                                 .setContentText(alarmNot != null && alarmNot.contains(loggedNotification) ? context.getString(R.string.alarm) + ": " + text : text)
-                                .setStyle(new NotificationCompat.BigTextStyle().bigText(text))
+                                .setChannelId(CHANNEL_ID)
                                 .setGroupSummary(true)
                                 .setGroup(GROUP_KEY_NOTIFICATIONS)
-                                .setPriority(prio)
+                                .setStyle(new NotificationCompat.BigTextStyle().bigText(text))
                                 .setAutoCancel(true);
 
                 if (!prefUtil.OverWriteNotifications())
@@ -126,30 +121,56 @@ public class NotificationUtil {
                             .setUnreadConversation(getUnreadConversation(context, text)));
                 }
 
-                NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build());
-                if (prefUtil.isNotificationsEnabled() && alarmNot != null && alarmNot.contains(loggedNotification)) {
-                    Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-                    if (alert == null) {
-                        alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                        if (alert == null)
-                            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-                    }
-
-                    if (alert != null) {
-                        Intent ringtoneServiceStartIntent = new Intent(context, RingtonePlayingService.class);
-                        ringtoneServiceStartIntent.putExtra("ringtone-uri", alert.toString());
-                        context.startService(ringtoneServiceStartIntent);
-
-                        if (prefUtil.getAlarmTimer() > 0) {
-                            Thread.sleep(prefUtil.getAlarmTimer() * 1000);
-                            Intent stopIntent = new Intent(context, RingtonePlayingService.class);
-                            context.stopService(stopIntent);
-                        }
-                    }
-                }
+                mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+                HandleAlarmSounds(context, loggedNotification, alarmNot);
             }
         } catch (Exception ex) {
             Log.i("NOTIFY", ex.getMessage());
+        }
+    }
+
+    private static int GetPriority(int priority) {
+        int prio = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            prio = NotificationManager.IMPORTANCE_DEFAULT;
+            switch (priority) {
+                case 1:
+                    prio = NotificationManager.IMPORTANCE_HIGH;
+                    break;
+                case 2:
+                    prio = NotificationManager.IMPORTANCE_MAX;
+                    break;
+                case -1:
+                    prio = NotificationManager.IMPORTANCE_LOW;
+                    break;
+                case -2:
+                    prio = NotificationManager.IMPORTANCE_MIN;
+                    break;
+            }
+        }
+        return prio;
+    }
+
+    private static void HandleAlarmSounds(Context context, String loggedNotification, List<String> alarmNot) throws InterruptedException {
+        if (prefUtil.isNotificationsEnabled() && alarmNot != null && alarmNot.contains(loggedNotification)) {
+            Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            if (alert == null) {
+                alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                if (alert == null)
+                    alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+
+            if (alert != null) {
+                Intent ringtoneServiceStartIntent = new Intent(context, RingtonePlayingService.class);
+                ringtoneServiceStartIntent.putExtra("ringtone-uri", alert.toString());
+                context.startService(ringtoneServiceStartIntent);
+
+                if (prefUtil.getAlarmTimer() > 0) {
+                    Thread.sleep(prefUtil.getAlarmTimer() * 1000);
+                    Intent stopIntent = new Intent(context, RingtonePlayingService.class);
+                    context.stopService(stopIntent);
+                }
+            }
         }
     }
 
