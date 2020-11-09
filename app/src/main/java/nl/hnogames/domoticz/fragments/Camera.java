4cbb0834edf331f28ec0411311ae5ce396e7f8b1
Mark Heinis <heinis.mark@gmail.com>
2020-11-09 12:55:00 +0100

    Reworked the storage of camera snapshots

--- app/src/main/java/nl/hnogames/domoticz/fragments/Camera.java
+++ app/src/main/java/nl/hnogames/domoticz/fragments/Camera.java
@@ -21,6 +21,7 @@
 
 package nl.hnogames.domoticz.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -42,7 +43,12 @@ import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import androidx.fragment.app.Fragment;
 import nl.hnogames.domoticz.R;
@@ -85,12 +91,7 @@ public class Camera extends Fragment {
                 .setGravity(Gravity.CENTER);
 
         FloatingActionButton fabButton = group.findViewById(R.id.fab);
-        fabButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                processImage();
-            }
-        });
+        fabButton.setOnClickListener(v -> processImage());
         if (idx > 0)
             setImage(idx);
         return group;
@@ -114,20 +115,45 @@ public class Camera extends Fragment {
         super.onActivityCreated(savedInstanceState);
     }
 
+    private static final int CREATE_SNAPSHOT_IMAGE = 111;
     private void processImage() {
         if (root != null) {
-            BitmapDrawable drawable = (BitmapDrawable) root.getDrawable();
-            Bitmap bitmap = drawable.getBitmap();
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/jpg");
+                intent.putExtra(Intent.EXTRA_TITLE, "snapshot_share-image.jpg");
+                startActivityForResult(intent, CREATE_SNAPSHOT_IMAGE);
+        }
+    }
 
-            File snapFile = StaticHelper.getDomoticz(mContext).saveSnapShot(bitmap, "share-image");
-            Uri uriBitmap = Uri.fromFile(snapFile);
-            if (uriBitmap != null) {
-                Intent shareIntent = new Intent();
-                shareIntent.setAction(Intent.ACTION_SEND);
-                shareIntent.putExtra(Intent.EXTRA_STREAM, uriBitmap);
-                shareIntent.setType("image/*");
-                startActivity(Intent.createChooser(shareIntent, "Share Image"));
-            }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if( requestCode == CREATE_SNAPSHOT_IMAGE ) {
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        if (data != null && data.getData() != null) {
+                            OutputStream outputStream;
+                            try {
+                                BitmapDrawable drawable = (BitmapDrawable) root.getDrawable();
+                                Bitmap bitmap = drawable.getBitmap();
+                                outputStream = mContext.getContentResolver().openOutputStream(data.getData());
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream);
+                                outputStream.flush();
+                                outputStream.close();
+
+                                Intent shareIntent = new Intent();
+                                shareIntent.setAction(Intent.ACTION_SEND);
+                                shareIntent.putExtra(Intent.EXTRA_STREAM, data.getData());
+                                shareIntent.setType("image/*");
+                                startActivity(Intent.createChooser(shareIntent, "Share Image"));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                }
         }
     }
 
