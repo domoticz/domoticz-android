5ead0f559010bcbd429e4e4429c8e2ee155be8ee
Mark Heinis <heinis.mark@gmail.com>
2020-12-07 20:51:01 +0100

    Working on a first version with Android 11 Custom Controls

--- app/src/main/java/nl/hnogames/domoticz/service/CustomControlService.java
+++ app/src/main/java/nl/hnogames/domoticz/service/CustomControlService.java
@@ -9,6 +9,11 @@ import android.service.controls.ControlsProviderService;
 import android.service.controls.DeviceTypes;
 import android.service.controls.actions.BooleanAction;
 import android.service.controls.actions.ControlAction;
+import android.service.controls.templates.ControlButton;
+import android.service.controls.templates.ControlTemplate;
+import android.service.controls.templates.RangeTemplate;
+import android.service.controls.templates.StatelessTemplate;
+import android.service.controls.templates.ToggleTemplate;
 import android.util.Log;
 
 import org.reactivestreams.FlowAdapters;
@@ -17,15 +22,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Flow;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import io.reactivex.Flowable;
 import io.reactivex.processors.ReplayProcessor;
-import nl.hnogames.domoticz.R;
-import nl.hnogames.domoticz.adapters.DashboardAdapter;
-import nl.hnogames.domoticz.adapters.SwitchesAdapter;
 import nl.hnogames.domoticz.helpers.StaticHelper;
+import nl.hnogames.domoticz.utils.DeviceUtils;
 import nl.hnogames.domoticzapi.Containers.DevicesInfo;
 import nl.hnogames.domoticzapi.Domoticz;
 import nl.hnogames.domoticzapi.DomoticzValues;
@@ -75,44 +79,62 @@ public class CustomControlService extends ControlsProviderService {
         ArrayList<DevicesInfo> supportedSwitches = new ArrayList<>();
         for (DevicesInfo mDevicesInfo : extendedStatusSwitches) {
             String name = mDevicesInfo.getName();
-            if (!name.startsWith(Domoticz.HIDDEN_CHARACTER)) {
+            if (!name.startsWith(Domoticz.HIDDEN_CHARACTER) && DeviceUtils.isSupportedForExternalControl(mDevicesInfo)) {
                 supportedSwitches.add(mDevicesInfo);
             }
         }
         for (DevicesInfo d: supportedSwitches) {
-            stateControls.add(new Control.StatefulBuilder(d.getIdx()+"", activityIntent)
+            Control control = (new Control.StatefulBuilder(d.getIdx()+"", activityIntent)
                     .setTitle(d.getName())
                     .setSubtitle(d.getDescription())
                     .setStructure(d.getPlanID())
+                    .setControlTemplate(getControlTemplate(d))
                     .setDeviceType(getDeviceType(d))
                     .setStatus(Control.STATUS_OK)
+                    .setStatusText(d.getStatus() != null ? d.getStatus() : "")
                     .build());
+            updateControl(control);
         }
     }
 
-    public int getDeviceType(DevicesInfo device)
+    private void processSwitch(DevicesInfo extendedStatusSwitches) {
+        ArrayList<DevicesInfo> supportedSwitches = new ArrayList<>();
+        supportedSwitches.add(extendedStatusSwitches);
+        for (DevicesInfo d: supportedSwitches) {
+            Control control = (new Control.StatefulBuilder(d.getIdx()+"", activityIntent)
+                    .setTitle(d.getName())
+                    .setSubtitle(d.getDescription())
+                    .setStructure(d.getPlanID())
+                    .setControlTemplate(getControlTemplate(d))
+                    .setDeviceType(getDeviceType(d))
+                    .setStatus(Control.STATUS_OK)
+                    .setStatusText(d.getStatus() != null ? d.getStatus() : "")
+                    .build());
+            updateControl(control);
+        }
+    }
+
+    public ControlTemplate getControlTemplate(DevicesInfo mDeviceInfo)
     {
-        if (device.getSwitchTypeVal() == 0 &&
-                (device.getSwitchType() == null)) {
-            switch (device.getType()) {
-                case DomoticzValues.Scene.Type.GROUP:
-                    return DeviceTypes.TYPE_LIGHT;
-
-                case DomoticzValues.Scene.Type.SCENE:
-                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
-
-                default:
-                    return DeviceTypes.TYPE_LIGHT;
-
+        if (mDeviceInfo.getSwitchTypeVal() == 0 &&
+                (mDeviceInfo.getSwitchType() == null)) {
+            if (mDeviceInfo.getSubType() != null && mDeviceInfo.getSubType().equals(DomoticzValues.Device.Utility.SubType.SMARTWARES)) {
+                return new StatelessTemplate(mDeviceInfo.getIdx() + "_stateless");
+            } else {
+                switch (mDeviceInfo.getType()) {
+                    case DomoticzValues.Scene.Type.GROUP:
+                        return new ToggleTemplate(mDeviceInfo.getIdx() + "_toggle", new ControlButton(mDeviceInfo.getStatusBoolean(), "toggle"));
+                    case DomoticzValues.Scene.Type.SCENE:
+                        return new StatelessTemplate(mDeviceInfo.getIdx() + "_stateless");
+                    case DomoticzValues.Device.Utility.Type.THERMOSTAT:
+                }
             }
-        }else {
-            switch (device.getSwitchTypeVal()) {
+        } else {
+            switch (mDeviceInfo.getSwitchTypeVal()) {
                 case DomoticzValues.Device.Type.Value.ON_OFF:
-                case DomoticzValues.Device.Type.Value.MEDIAPLAYER:
                 case DomoticzValues.Device.Type.Value.DOORLOCK:
-                case DomoticzValues.Device.Type.Value.DOORLOCKINVERTED:
-                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
-
+                case DomoticzValues.Device.Type.Value.DOORCONTACT:
+                    return new ToggleTemplate(mDeviceInfo.getIdx() + "_toggle", new ControlButton(mDeviceInfo.getStatusBoolean(), "toggle"));
 
                 case DomoticzValues.Device.Type.Value.X10SIREN:
                 case DomoticzValues.Device.Type.Value.MOTION:
@@ -120,63 +142,99 @@ public class CustomControlService extends ControlsProviderService {
                 case DomoticzValues.Device.Type.Value.DUSKSENSOR:
                 case DomoticzValues.Device.Type.Value.SMOKE_DETECTOR:
                 case DomoticzValues.Device.Type.Value.DOORBELL:
-                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
-
                 case DomoticzValues.Device.Type.Value.PUSH_ON_BUTTON:
-                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
-
-
                 case DomoticzValues.Device.Type.Value.PUSH_OFF_BUTTON:
-                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
+                    return new StatelessTemplate(mDeviceInfo.getIdx() + "_stateless");
 
-
-                case DomoticzValues.Device.Type.Value.DOORCONTACT:
-                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
-
-
-                case DomoticzValues.Device.Type.Value.DIMMER:
-                    return DeviceTypes.TYPE_LIGHT;
-
-
+                    case DomoticzValues.Device.Type.Value.DIMMER:
                 case DomoticzValues.Device.Type.Value.BLINDPERCENTAGE:
                 case DomoticzValues.Device.Type.Value.BLINDPERCENTAGEINVERTED:
-                    return DeviceTypes.TYPE_BLINDS;
-
-
                 case DomoticzValues.Device.Type.Value.BLINDS:
                 case DomoticzValues.Device.Type.Value.BLINDINVERTED:
-                    return DeviceTypes.TYPE_BLINDS;
+                case DomoticzValues.Device.Type.Value.BLINDVENETIAN:
+                case DomoticzValues.Device.Type.Value.BLINDVENETIANUS:
+                    int maxValue = mDeviceInfo.getMaxDimLevel() <= 0 ? 100 : mDeviceInfo.getMaxDimLevel();
+                    return new RangeTemplate(mDeviceInfo.getIdx() + "_range",0.0f,
+                            maxValue,
+                            mDeviceInfo.getLevel() > maxValue ? maxValue : mDeviceInfo.getLevel(),
+                            1, "%d");
+                default:
+                    return new StatelessTemplate(mDeviceInfo.getIdx() + "_stateless");
+            }
+        }
+        return new StatelessTemplate(mDeviceInfo.getIdx() + "_stateless");
+    }
 
-
+    public int getDeviceType(DevicesInfo mDeviceInfo) {
+        if (mDeviceInfo.getSwitchTypeVal() == 0 &&
+                (mDeviceInfo.getSwitchType() == null)) {
+            if (mDeviceInfo.getSubType() != null && mDeviceInfo.getSubType().equals(DomoticzValues.Device.Utility.SubType.SMARTWARES)) {
+                return DeviceTypes.TYPE_GENERIC_ON_OFF;
+            } else {
+                switch (mDeviceInfo.getType()) {
+                    case DomoticzValues.Scene.Type.GROUP:
+                        return DeviceTypes.TYPE_LIGHT;
+                    case DomoticzValues.Scene.Type.SCENE:
+                        return DeviceTypes.TYPE_GENERIC_ON_OFF;
+                    case DomoticzValues.Device.Utility.Type.THERMOSTAT:
+                }
+            }
+        } else {
+            switch (mDeviceInfo.getSwitchTypeVal()) {
+                case DomoticzValues.Device.Type.Value.ON_OFF:
+                case DomoticzValues.Device.Type.Value.MEDIAPLAYER:
+                case DomoticzValues.Device.Type.Value.DOORLOCK:
+                case DomoticzValues.Device.Type.Value.DOORCONTACT:
+                    switch (mDeviceInfo.getSwitchType()) {
+                        case DomoticzValues.Device.Type.Name.SECURITY:
+                            return -1;
+                        default:
+                            return DeviceTypes.TYPE_GENERIC_ON_OFF;
+                    }
+                case DomoticzValues.Device.Type.Value.X10SIREN:
+                case DomoticzValues.Device.Type.Value.MOTION:
+                case DomoticzValues.Device.Type.Value.CONTACT:
+                case DomoticzValues.Device.Type.Value.DUSKSENSOR:
+                case DomoticzValues.Device.Type.Value.SMOKE_DETECTOR:
+                case DomoticzValues.Device.Type.Value.DOORBELL:
+                case DomoticzValues.Device.Type.Value.PUSH_ON_BUTTON:
+                case DomoticzValues.Device.Type.Value.PUSH_OFF_BUTTON:
+                    return DeviceTypes.TYPE_GENERIC_ON_OFF;
+                case DomoticzValues.Device.Type.Value.DIMMER:
+                    return DeviceTypes.TYPE_LIGHT;
+                case DomoticzValues.Device.Type.Value.BLINDPERCENTAGE:
+                case DomoticzValues.Device.Type.Value.BLINDPERCENTAGEINVERTED:
+                case DomoticzValues.Device.Type.Value.BLINDS:
+                case DomoticzValues.Device.Type.Value.BLINDINVERTED:
                 case DomoticzValues.Device.Type.Value.BLINDVENETIAN:
                 case DomoticzValues.Device.Type.Value.BLINDVENETIANUS:
                     return DeviceTypes.TYPE_BLINDS;
-
-
                 default:
-                    return DeviceTypes.TYPE_LIGHT;
+                    return -1;
             }
         }
+        return DeviceTypes.TYPE_GENERIC_ON_OFF;
+    }
+
+    private void updateControl(Control control) {
+        stateControls = stateControls.stream().filter(c -> !c.getControlId().equals(control.getControlId())).collect(Collectors.toList());
+        stateControls.add(control);
+        if (updatePublisher != null)
+            updatePublisher.onNext(control);
     }
 
     @NonNull
     @Override
     public Flow.Publisher<Control> createPublisherFor(@NonNull List<String> controlIds) {
-        Context context = getBaseContext();
-        Intent i = new Intent();
-        PendingIntent pi = PendingIntent.getActivity(context, 1, i, PendingIntent.FLAG_UPDATE_CURRENT);
         updatePublisher = ReplayProcessor.create();
-
-        // For each controlId in controlIds
-        if (controlIds.contains("domoticz1")) {
-            Control control = new Control.StatefulBuilder("domoticz1", pi)
-                    .setTitle("Lights")
-                    .setSubtitle("Living room")
-                    .setStructure("House")
-                    .setDeviceType(DeviceTypes.TYPE_LIGHT)
-                    .setStatus(Control.STATUS_OK)
-                    .build();
-            updatePublisher.onNext(control);
+        getSwitches();
+        for(String id : controlIds)
+        {
+            Log.i(TAG, "cpf "+ id);
+            for (DevicesInfo d: extendedStatusSwitches) {
+                if((d.getIdx()+"").equals(id))
+                    processSwitch(d);
+            }
         }
         return FlowAdapters.toFlowPublisher(updatePublisher);
     }
