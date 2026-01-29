aef0589a070215733a18650285e7d73f9a9d4e3e
Galadril <heinis.mark@gmail.com>
2026-01-29 19:06:40 +0100

    WIP Android Auto changes

--- app/src/main/java/nl/hnogames/domoticz/service/AutoMainScreen.java
+++ app/src/main/java/nl/hnogames/domoticz/service/AutoMainScreen.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.car.app.CarContext;
 import androidx.car.app.Screen;
 import androidx.car.app.model.Action;
-import androidx.car.app.model.ActionStrip;
+import androidx.car.app.model.CarIcon;
+import androidx.car.app.model.GridItem;
+import androidx.car.app.model.GridTemplate;
+import androidx.car.app.model.Header;
 import androidx.car.app.model.ItemList;
-import androidx.car.app.model.ListTemplate;
-import androidx.car.app.model.Row;
 import androidx.car.app.model.Template;
+import androidx.core.graphics.drawable.IconCompat;
 
 import nl.hnogames.domoticz.R;
 
@@ -25,52 +27,65 @@ public class AutoMainScreen extends Screen {
     @NonNull
     @Override
     public Template onGetTemplate() {
-        ItemList menuList = new ItemList.Builder()
-                .addItem(new Row.Builder()
-                        .setTitle(getCarContext().getString(R.string.title_dashboard))
-                        .addText(getCarContext().getString(R.string.category_dashboard_subtitle))
-                        .setOnClickListener(() -> navigateToDashboard())
-                        .build())
-                .addItem(new Row.Builder()
-                        .setTitle(getCarContext().getString(R.string.title_switches))
-                        .addText(getCarContext().getString(R.string.category_switches_subtitle))
-                        .setOnClickListener(() -> navigateToSwitches())
-                        .build())
-                .addItem(new Row.Builder()
-                        .setTitle(getCarContext().getString(R.string.title_temperature))
-                        .addText(getCarContext().getString(R.string.category_temperature_subtitle))
-                        .setOnClickListener(() -> navigateToTemperature())
-                        .build())
-                .addItem(new Row.Builder()
-                        .setTitle(getCarContext().getString(R.string.title_weather))
-                        .addText(getCarContext().getString(R.string.category_weather_subtitle))
-                        .setOnClickListener(() -> navigateToWeather())
-                        .build())
-                .addItem(new Row.Builder()
-                        .setTitle(getCarContext().getString(R.string.title_utilities))
-                        .addText(getCarContext().getString(R.string.category_utilities_subtitle))
-                        .setOnClickListener(() -> navigateToUtilities())
-                        .build())
-                .addItem(new Row.Builder()
-                        .setTitle(getCarContext().getString(R.string.title_scenes))
-                        .addText(getCarContext().getString(R.string.category_scenes_subtitle))
-                        .setOnClickListener(() -> navigateToScenes())
-                        .build())
-                .build();
+        ItemList.Builder gridBuilder = new ItemList.Builder();
 
-        return new ListTemplate.Builder()
-                .setSingleList(menuList)
-                .setTitle(getCarContext().getString(R.string.app_name_domoticz))
-                .setHeaderAction(Action.APP_ICON)
-                .setActionStrip(buildActionStrip())
-                .build();
-    }
+        // Dashboard - gmd_dashboard
+        gridBuilder.addItem(new GridItem.Builder()
+                .setImage(new CarIcon.Builder(IconCompat.createWithResource(
+                        getCarContext(), R.drawable.ic_auto_dashboard)).build())
+                .setTitle(getCarContext().getString(R.string.title_dashboard))
+                .setOnClickListener(this::navigateToDashboard)
+                .build());
 
-    private ActionStrip buildActionStrip() {
-        return new ActionStrip.Builder()
-                .addAction(new Action.Builder()
-                        .setTitle(getCarContext().getString(R.string.refresh))
-                        .setOnClickListener(this::refresh)
+        // Switches - gmd_lightbulb_outline
+        gridBuilder.addItem(new GridItem.Builder()
+                .setImage(new CarIcon.Builder(IconCompat.createWithResource(
+                        getCarContext(), R.drawable.ic_auto_lightbulb)).build())
+                .setTitle(getCarContext().getString(R.string.title_switches))
+                .setOnClickListener(this::navigateToSwitches)
+                .build());
+
+        // Scenes - gmd_view_carousel
+        gridBuilder.addItem(new GridItem.Builder()
+                .setImage(new CarIcon.Builder(IconCompat.createWithResource(
+                        getCarContext(), R.drawable.ic_auto_scenes)).build())
+                .setTitle(getCarContext().getString(R.string.title_scenes))
+                .setOnClickListener(this::navigateToScenes)
+                .build());
+
+        // Temperature - gmd_opacity
+        gridBuilder.addItem(new GridItem.Builder()
+                .setImage(new CarIcon.Builder(IconCompat.createWithResource(
+                        getCarContext(), R.drawable.ic_auto_temperature)).build())
+                .setTitle(getCarContext().getString(R.string.title_temperature))
+                .setOnClickListener(this::navigateToTemperature)
+                .build());
+
+        // Weather - gmd_wb_sunny
+        gridBuilder.addItem(new GridItem.Builder()
+                .setImage(new CarIcon.Builder(IconCompat.createWithResource(
+                        getCarContext(), R.drawable.ic_auto_weather)).build())
+                .setTitle(getCarContext().getString(R.string.title_weather))
+                .setOnClickListener(this::navigateToWeather)
+                .build());
+
+        // Utilities - gmd_dvr
+        gridBuilder.addItem(new GridItem.Builder()
+                .setImage(new CarIcon.Builder(IconCompat.createWithResource(
+                        getCarContext(), R.drawable.ic_auto_utilities)).build())
+                .setTitle(getCarContext().getString(R.string.title_utilities))
+                .setOnClickListener(this::navigateToUtilities)
+                .build());
+
+        return new GridTemplate.Builder()
+                .setSingleList(gridBuilder.build())
+                .setHeader(new Header.Builder()
+                        .setTitle(getCarContext().getString(R.string.app_name_domoticz))
+                        .setStartHeaderAction(Action.APP_ICON)
+                        .addEndHeaderAction(new Action.Builder()
+                                .setTitle(getCarContext().getString(R.string.refresh))
+                                .setOnClickListener(this::refresh)
+                                .build())
                         .build())
                 .build();
     }
